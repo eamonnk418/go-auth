e6608edca595120a4802194f5df2fbbe615cd08e
eamonnk418 <160419984+eamonnk418@users.noreply.github.com>
2025-04-06 20:42:12 +0100

    wip

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,8 +5,11 @@ import (
 	"github.com/eamonnk418/go-auth/internal/server"
 )
 
-func main() {
+func init() {
 	auth.NewAuth()
+}
+
+func main() {
 	server := server.NewServer(nil)
 
 	if err := server.Start(":8080"); err != nil {
